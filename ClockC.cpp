69e076ab0c31506bd4eef57f354e6742e9f036af
kanaqw <81157913+kanaqw@users.noreply.github.com>
2021-04-14 17:10:46 +0700

    Update ClockC.cpp

--- ClockC.cpp
+++ ClockC.cpp
@@ -1,21 +1,31 @@
+#include <iostream>
+#include <cstdio>
+
+
 struct Clockofthelongnow {
-	
+
 	bool set_year(int year) {
 		if (year < 2019) return false;
 		this->year = year;
 		return true;
 	}
-	int get_year() {
+	int get_year() const {
 		return year;
 	}
-	int year=2020;
+	int year = 2020;
 };
 
 
 void add_year(Clockofthelongnow& clock) {
 	clock.set_year(clock.get_year() + 1);
-	}
+}
 
+bool is_leap_year(const Clockofthelongnow& clock) {
+	if (clock.get_year() % 4 > 0)return false;
+	if (clock.get_year() % 100 > 0)return true;
+	if (clock.get_year() % 400 > 0) return false;
+	return true;
+}
 
 int main() {
 	Clockofthelongnow clock;
