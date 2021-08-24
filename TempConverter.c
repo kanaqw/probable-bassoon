020f8aa1cc61f537064241c80032f1e676207cae
kanaqw <81157913+kanaqw@users.noreply.github.com>
2021-08-24 20:03:50 +0300

    Update TempConverter.c

--- TempConverter.c
+++ TempConverter.c
@@ -4,20 +4,21 @@
 void Temperatures(double tem);
 
 int main(void) {
-    printf("Enter temp in F: \n");
-    double far;
-    int input=scanf("%lf", &far);
+    printf("Enter temp in C: \n");
+    printf("Or press q to exit\n")
+    double celc;
+    int input=scanf("%lf", &celc);
     while (input==1) {
-        Temperatures(far);
-        scanf("%lf", &far);
+        Temperatures(celc);
+        scanf("%lf", &celc);
     }
 }
 
 void Temperatures(double tem) {
 
-    const double celc = 5.0/9.0*(tem-32.0);
-    const double kelv = celc+273.16;
-    printf("temp C = %.2f\n", celc);
+    const double far = (tem*9/5)+32;
+    const double kelv = tem+273.16;
+    printf("temp F = %.2f\n", far);
     printf("temp K = %.2f\n", kelv);
     printf("Enter temp in F: \n");
 
