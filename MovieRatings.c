c8e4012ee674ec7bb1de1f0116dbd96c23b5a99b
kanaqw <81157913+kanaqw@users.noreply.github.com>
2022-10-05 20:00:42 +0500

    Update MovieRatings.c
    updated with ADT

--- MovieRatings.c
+++ MovieRatings.c
@@ -1,56 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#define TSIZE 45
+#include "MovieList.h"
 
-struct film {
-    char title[TSIZE];
-    int rating;
-    struct film* next;
-};
+void showMovies(Item item);
+char * s_gets(char *st,int n);
 
-char s_gets(char* st,int n);
+static void List_Init(List *plist);
+static bool ListIsEmpty(const List *plist);
+static bool ListIsFull(const List *plist);
+static unsigned int ListItemCount(const List *plist);
+static bool AddItem(Item item, List *plist);
+static void Traverse(const List* plist, void (*pfun)(Item item));
+static void EmptyTheList(List *plist);
+
 
 int main(void){
-    struct film* head = NULL;
-    struct film* prev, *current;
-    char input[TSIZE];
 
-    puts("First Movie name");
-    while (s_gets(input, TSIZE) != NULL && input[0] != '\0'){
-        current = (struct film *) malloc(sizeof(struct film));
-        if (head == NULL)
-            head = current;
-        else 
-            prev->next = current;
-            current->next = NULL;
-            strcpy(current->title, input);
-            puts("Your rating (0-10):");
-            scanf(%d, &current->rating);
-            while(getchar() != '\n')
-                continue;
-            puts("Next movie name (or empty string to stop input): ");
-                prev = current;
+    List movies;
+    Item temp;
+    List_Init(&movies);
+    if (ListIsFull(&movies)){
+        fprintf(stderr, "ERROR: No memory available!\n");
+        exit(1);
     }
 
-    if (head == NULL)
-        printf("No data found");
-    else
-        printf("movie list:\n");
-    current = head;
-    while (current != NULL) {
-        printf("Movie %s Rating %d", current->title, current->rating); 
-        current = current->next;
-    }
-    current = head;
-    while (current != NULL){
-        current = head;
-        head = current->next;
-        free(current);
+    puts("First movie name: ");
+    while (s_gets(temp.title, TSIZE) != NULL && temp.title[0] != '\0'){
+        puts("Your rating (0-10): ");
+        scanf("%d", &temp.rating);
+        while (getchar() != '\n');
+            continue;
+        if (AddItem(temp, &movies) == false){
+            fprintf(stderr, "ERROR: Memory allocation error!\n");
+            break;
+        }
+        if (ListIsFull(&movies)){
+            puts("List is full");
+            break;
+        }
+        puts("Enter next movie name: (or empty string to finish)");
 
     }
+
+    if (ListIsEmpty(&movies))
+        printf("No data entered");
+    else{
+        printf("Movie list: \n");
+        Traverse(&movies, showMovies);
+    } 
+    printf("You entered %d movies. \n", ListItemCount(&movies));
+
+    EmptyTheList(&movies);
     printf("Done\n");
     return 0;
+
+}
+
+void showMovies(Item item){
+    printf("Movie: %s Rating : %d", item.title, item.rating);
 }
 
 char * s_gets(char *st, int  n){
@@ -69,4 +76,81 @@ char * s_gets(char *st, int  n){
         }
         return ret_val;
     }
+}
+
+//interface functions - should be in separate file, but I am lazy
+static void CopyToNode(Item item, Node *pnode);
+void List_Init(List *plist){
+    *plist = NULL;
+}
+
+static bool ListIsEmpty(const List *plist){
+    if (*plist == NULL)
+        return true;
+    else 
+        return false;
+}
+
+static bool ListIsFull(const List* plist){
+    Node *pt;
+    bool full;
+    pt = (Node*) malloc(sizeof(Node));
+    if (pt == NULL)
+        full = true;
+    else 
+        full = false;
+    free(pt);
+    return full;
+}
+
+static unsigned int ListItemCount(const List *plist){
+    unsigned int count = 0;
+    Node *pnode = *plist;
+    while (pnode != NULL){
+        ++count;
+        pnode = pnode->next;
+    }
+    return count;
+
+}
+
+static bool AddItem(Item item, List *plist){
+    Node *pnew;
+    Node *scan = *plist;
+
+    pnew = (Node*) malloc(sizeof(Node));
+    if (pnew == NULL)
+        return false;
+    CopyToNode(item, pnew);
+    pnew->next = NULL;
+    if (scan == NULL)
+        *plist = pnew;
+    else{
+            while(scan->next != NULL)
+                scan = scan->next;
+            scan->next = pnew;
+        }
+    return true;
+}
+
+static void Traverse(const List* plist, void (*pfun)(Item item)){
+    Node *pnode = *plist;
+    while (pnode != NULL){
+        (*pfun)(pnode->item);
+        pnode = pnode->next;
+
+    }
+}
+
+static void EmptyTheList(List *plist){
+    Node *psave;
+    while (*plist != NULL){
+        psave = (*plist)->next;
+        free(*plist);
+        *plist = psave;
+    }
+}
+
+static void CopyToNode(Item item, Node* pnode){
+    pnode->item = item;
 }
